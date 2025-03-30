24b0316631dfe9f7df0da599ecf9773d59dd23a8
jakkulavenkatarahul11 <153607406+jakkulavenkatarahul11@users.noreply.github.com>
2025-03-30 16:22:32 +0530

    Time: 1 ms (47.01%), Space: 9 MB (26.99%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        // Step 1: Store the last occurrence of each character
+        unordered_map<char, int> lastOccurrence;
+        for (int index = 0; index < s.length(); index++) {
+            lastOccurrence[s[index]] = index;
+        }
+
+        vector<int> partitionSizes;
+        int partitionStart = 0, partitionEnd = 0;
+
+        // Step 2: Iterate and determine partitions
+        for (int index = 0; index < s.length(); index++) {
+            partitionEnd = max(partitionEnd, lastOccurrence[s[index]]);
+            if (index == partitionEnd) {
+                partitionSizes.push_back(partitionEnd - partitionStart + 1);
+                partitionStart = index + 1;
+            }
+        }
+        return partitionSizes;
+    }
+};
